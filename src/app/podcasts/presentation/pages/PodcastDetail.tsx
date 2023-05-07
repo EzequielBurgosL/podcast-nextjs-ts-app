28face68bf47d73bd66398160cd3b85067371014
Ezequiel Burgos <ezbulo@gmail.com>
2023-05-07 19:08:43 +0200

    feature: add some styles to episodes table

--- src/app/podcasts/presentation/pages/PodcastDetail.tsx
+++ src/app/podcasts/presentation/pages/PodcastDetail.tsx
@@ -26,28 +26,37 @@ export const PodcastDetail: React.FC = ({ data }: { data: Props }) => {
         author={data.author}
         imageUrl={data.imageUrl}
       />
-      {!!episodes.length && (
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Duration</th>
-              <th>Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {episodes.map(episode => {
-              return (
-                <tr key={episode.id}>
-                  <td>{episode.title}</td>
-                  <td>{episode.duration}</td>
-                  <td>{episode.date}</td>
+      <div className="detail-page-content">
+        <div className="episodes-counter">
+          <h1>Episodes: {episodes.length}</h1>
+        </div>
+        <div className="episodes-table">
+          {!!episodes.length && (
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Date</th>
+                  <th>Duration</th>
                 </tr>
-              )
-            })}
-          </tbody>
-        </table>
-      )}
+              </thead>
+              <tbody>
+                {episodes.map((episode, index) => {
+                  const rowClass = index % 2 === 0 ? 'even-row' : 'odd-row';
+
+                  return (
+                    <tr key={episode.id} className={rowClass}>
+                      <td>{episode.title}</td>
+                      <td>{episode.date}</td>
+                      <td>{episode.duration}</td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
     </div>
   )
 };
