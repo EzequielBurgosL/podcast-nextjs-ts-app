b4859d8453bd180c085ebb6db0a0d2bdeef88dfe
Ezequiel Burgos <ezbulo@gmail.com>
2023-05-07 09:39:09 +0200

    feature: add input to filter podcasts

--- src/app/podcasts/presentation/pages/PodcastPage.tsx
+++ src/app/podcasts/presentation/pages/PodcastPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Podcast } from '../../domain/podcast';
 import { Card } from '../components/Card';
 
@@ -7,11 +7,33 @@ type Props = {
 };
 
 export const PodcastPage: React.FC<Props> = ({ data }) => {
+  const [filterText, setFilterText] = useState('');
+
+  const filteredPodcasts = data.filter(podcast =>
+    `${podcast.title} ${podcast.author}`
+    .toLowerCase()
+    .includes(filterText.toLowerCase())
+  );
+
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterText(event.target.value);
+  };
+
   return (
     <div className="page-container">
       <h1 className="page-title">Podcaster</h1>
+      <div className="filter">
+        <label htmlFor="filter-input">{filteredPodcasts.length}</label>
+        <input
+          type="text"
+          id="filter-input"
+          placeholder="Filter podcasts..."
+          value={filterText}
+          onChange={handleFilterChange}
+        />
+      </div>
       <div className="cards">
-        {data.map((podcast) => {
+        {filteredPodcasts.map((podcast) => {
           return <Card
             title={podcast.title}
             author={podcast.author}
